792fcd4a738572f94aa22e98e3ef06f7256b3008
Yampier Medina <jpmti2016@gmail.com>
2024-07-11 16:39:56 -0400

    webpack config in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 
-export default nextConfig;
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
